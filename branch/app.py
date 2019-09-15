ea5ff797e4ad750d348103012075aac4e3e384dc
Ravi Mishra <ravi.mishra710@gmail.com>
2019-09-15 15:18:53 +0530

    Updated dependencies in app.py

--- branch/app.py
+++ branch/app.py
@@ -4,13 +4,14 @@ from flask import Flask
 from branch.database import db
 from branch.views import branch
 
+app = Flask(__name__)
+
 
 def init_logger():
     logging.basicConfig(filename='error.log', level=logging.DEBUG)
 
 
 def create_app():
-    app = Flask(__name__)
     app.config.from_object(os.environ['APP_SETTINGS'])
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     db.init_app(app)
